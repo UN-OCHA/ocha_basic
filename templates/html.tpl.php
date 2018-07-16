8b64298bcc990c8fb41eaee1506e71378beab06f
Chris Ruppel <chris@humanitarianresponse.info>
2018-07-16 11:37:10 +0200

    :bug: remove buggy scripts-to-bottom implementation

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -13,6 +13,7 @@
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
+  <?php print $scripts; ?>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600" rel="stylesheet">
   <!--[if lt IE 9]>
@@ -23,6 +24,5 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
-  <?php print $scripts; ?>
 </body>
 </html>
