70b539cc05a1b07b03251b6cafddff9e50b8a87f
left23 <lcdemc@gmail.com>
2018-07-16 09:56:44 +0200

    remove experimental preprocess for menu, add custom js for dropdown based on d7 main menu markup output

--- js/custom-menu.js
+++ js/custom-menu.js
@@ -2,8 +2,12 @@
   Drupal.behaviors.exampleModule = {
     attach: function (context, settings) {
 
+      $('.cd-dropdown .expanded a').on('click', function(e){
+        $(this).parent().toggleClass('open');
+        e.stopPropagation();
+        e.preventDefault();
+      });
 
-      
     }
   };
 }(jQuery));
