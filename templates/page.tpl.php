87582f0a653468eb2683473b689528685175d15b
left23 <lcdemc@gmail.com>
2018-07-25 15:40:35 +0200

    remove non-styleguide wrapper divs. This breaks layout of sidebar WIP

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -7,7 +7,7 @@
  * @see https://drupal.org/node/1728148
  */
 ?>
-<div class="cd-page-wrapper">
+<div class="page-wrapper">
   <a href="#main-content" class="skip-link"><?php print t('Skip to main content'); ?></a>
   <?php include 'header.inc'; ?>
   <?php if($messages): ?>
@@ -15,6 +15,7 @@
       <?php print $messages; ?>
     </div>
   <?php endif; ?>
+  
   <div class="cd-container" id="main-content">
 
   <?php if($tabs): ?>
@@ -22,18 +23,14 @@
 
   <?php endif; ?>
 
-    <div class="row">
-      <div class="<?php if ($page['sidebar_first']): ?>col-md-8<?php else: ?>col-xs-12<?php endif; ?>">
+
         <?php if ($title): ?><h1 class="page-heading"><?php print $title; ?></h1><?php endif; ?>
         <?php print render($page['content']); ?>
-      </div>
 
       <?php if ($page['sidebar_first']): ?>
-        <div class="col-md-4">
           <?php print render($page['sidebar_first']); ?>
-        </div>
       <?php endif; ?>
-    </div>
+
   </div>
 </div>
 <?php include 'footer.inc'; ?>
