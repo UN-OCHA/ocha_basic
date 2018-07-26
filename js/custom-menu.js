3839c3dc2c7ae5049b1ac4a43999137ebf1a1803
left23 <lcdemc@gmail.com>
2018-07-26 14:41:55 +0200

    remove main menu preprocess, hurray

--- js/custom-menu.js
+++ js/custom-menu.js
@@ -2,7 +2,7 @@
   Drupal.behaviors.exampleModule = {
     attach: function (context, settings) {
 
-      $('.cd-dropdown .cd-dropdown-toggle a').on('click', function(e){
+      $('.cd-dropdown .expanded a').on('click', function(e){
         $(this).parent().toggleClass('open');
         e.stopPropagation();
         e.preventDefault();
