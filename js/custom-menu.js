9304d770c91bd0ae9dbc6a7c243fa32eda5ef9f9
Chris Ruppel <chris@humanitarianresponse.info>
2018-08-13 13:33:49 +0200

    Apply whitespace settings to js directory

--- js/custom-menu.js
+++ js/custom-menu.js
@@ -6,13 +6,11 @@
 (function ($, Drupal) {
   Drupal.behaviors.cdMenu = {
     attach: function (context, settings) {
-
       $('.cd-dropdown .expanded a').on('click', function (e) {
         $(this).parent().toggleClass('open');
         e.stopPropagation();
         e.preventDefault();
       });
-
     }
   };
 }(jQuery, Drupal));
