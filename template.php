3d20af1b0b583dc3b7fa6a856fca94f26b141d66
left23 <lcdemc@gmail.com>
2018-08-21 10:09:12 +0200

    inline svg for search toggle and search submit button

--- template.php
+++ template.php
@@ -18,7 +18,8 @@ function ocha_basic_form_alter(&$form, &$form_state, $form_id) {
     $form['search_block_form']['#attributes']['class'][] = 'cd-search__input';
     $form['actions']['submit'] = array(
       '#type' => 'submit',
-      '#prefix' => '<button type="submit" id="edit-submit" name="op" class="cd-search__submit form-submit"><span class="icon-search" aria-hidden="true"></span><span class="element-invisible">Search</span>',
+      '#prefix' => '<button type="submit" id="edit-submit" name="op" class="cd-search__submit form-submit"><svg width="32" height="32" viewBox="0 0 32 32">
+<title>search</title><path d="M19.427 20.427c-1.39 0.99-3.090 1.573-4.927 1.573-4.694 0-8.5-3.806-8.5-8.5s3.806-8.5 8.5-8.5c4.694 0 8.5 3.806 8.5 8.5 0 2.347-0.951 4.472-2.49 6.010l5.997 5.997c0.275 0.275 0.268 0.716-0.008 0.992-0.278 0.278-0.72 0.28-0.992 0.008l-6.081-6.081zM14.5 21c4.142 0 7.5-3.358 7.5-7.5s-3.358-7.5-7.5-7.5c-4.142 0-7.5 3.358-7.5 7.5s3.358 7.5 7.5 7.5v0z"></path></svg><span class="element-invisible">Search</span>',
       '#suffix' => '</button>',
       '#markup' => '',
       '#weight' => 1000,
