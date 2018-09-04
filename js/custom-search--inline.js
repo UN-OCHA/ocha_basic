bf78484458be5191c7c15ba821dcc615ef05203f
left23 <lcdemc@gmail.com>
2018-09-04 14:35:30 +0200

    refactor jquery for search and search--inline based on code review

--- js/custom-search--inline.js
+++ js/custom-search--inline.js
@@ -6,35 +6,23 @@
 (function ($, Drupal) {
   Drupal.behaviors.cdSearchInline = {
     attach: function (context, settings) {
-      $(document).ready(function(){
 
-        // Apply focus to input when dropdown is shown.
-        $('.cd-search--inline').on('shown.bs.dropdown', function () {
-          $(this).find('#edit-search-api-views-fulltext').focus();
-        });
-
-        $('.cd-search--inline').on('hidden.bs.dropdown', function () {
-          $(this).find('#edit-search-api-views-fulltext').blur();
-        });
-
-        // Add class on submit button when input has focus.
-        $('.cd-search--inline .cd-search--inline__input').on('focus', function(e) {
-          $(this).parents().find('.cd-search--inline__submit').addClass('js-has-focus');
-        });
-
-        $('.cd-search--inline .cd-search--inline__input').on('blur', function(e) {
-          $(this).parents().find('.cd-search--inline__submit').removeClass('js-has-focus');
-        });
-
-        $('.cd-search--inline .cd-search--inline__input').on('focus', function(e){
-          $(this).parents().addClass('js-has-focus');
-        });
-
-        $('.cd-search--inline .cd-search--inline__input').on('blur', function(e){
-          $(this).parents().removeClass('js-has-focus');
-        });
-
+      // Apply focus to input when dropdown is shown, remove when closed.
+      $('.cd-search--inline').on('shown.bs.dropdown', function () {
+        $(this).find('#edit-search-api-views-fulltext').focus();
+      }).on('hidden.bs.dropdown', function () {
+        $(this).find('#edit-search-api-views-fulltext').blur();
       });
+
+      // Add class on submit button when input has focus, remove on blur.
+      $('.cd-search--inline .cd-search--inline__input').on('focus', function(e) {
+        $(this).parent().find('.cd-search--inline__submit').addClass('js-has-focus');
+        $(this).parent().addClass('js-has-focus');
+      }).on('blur', function(e) {
+        $(this).parent().find('.cd-search--inline__submit').removeClass('js-has-focus');
+        $(this).parent().removeClass('js-has-focus');
+      });
+
     }
   };
 }(jQuery, Drupal));
