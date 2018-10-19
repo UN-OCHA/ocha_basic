27f03064bc9db2a7fa0a881e5690dc52e75a99a6
left23 <lcdemc@gmail.com>
2018-10-19 13:43:34 +0200

    extend js to toggle aria-expanded on all menu levels, and prevent multiple menu item dropdowns being open at once

--- js/custom-menu.js
+++ js/custom-menu.js
@@ -6,11 +6,32 @@
 (function ($, Drupal) {
   Drupal.behaviors.cdMenu = {
     attach: function (context, settings) {
-      $('.cd-dropdown .expanded > a').on('click', function (e) {
-        $(this).parent().toggleClass('open');
+
+      $('.cd-nav .expanded > a').on('click', function (e) {
+
+        var menuItem = $( e.currentTarget );
+        if (menuItem.attr( 'aria-expanded') === 'true') {
+          $(this).attr( 'aria-expanded', 'false');
+        } else {
+          $(this).attr( 'aria-expanded', 'true');
+        }
         e.stopPropagation();
         e.preventDefault();
       });
+
+      // Prevent multiple dropdowns being open at once.
+      // First level.
+      $('.cd-nav > .menu > .expanded > a').on('click', function (e) {
+        $('.cd-nav > .menu > .expanded > a').not(this).parent().removeClass('open');
+        $(this).parent().toggleClass('open');
+      });
+      // Second level.
+      $('.cd-nav .menu .menu > .expanded > a').on('click', function (e) {
+        $(this).parent().toggleClass('open');
+      });
+
+      $('.dropdown').not(this).parent().removeClass('open');
+
     }
   };
 }(jQuery, Drupal));
