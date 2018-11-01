e24174b8cd4fafd3d3c8bb9dde0403018699231b
left23 <lcdemc@gmail.com>
2018-11-01 15:35:47 +0100

    detect viewport width on load and resize to add class to mobile menu for less than 1023px. When other dropdowns have class, close mobile menu. Emulates bootstrap dropdown behaviour for mobile only

--- js/custom-menu.js
+++ js/custom-menu.js
@@ -7,30 +7,32 @@
   Drupal.behaviors.cdMenu = {
     attach: function (context, settings) {
 
-      // $('.cd-nav .expanded > a').on('click', function (e) {
-      //
-      //   var menuItem = $( e.currentTarget );
-      //   if (menuItem.attr( 'aria-expanded') === 'true') {
-      //     $(this).attr( 'aria-expanded', 'false');
-      //   } else {
-      //     $(this).attr( 'aria-expanded', 'true');
-      //   }
-      //   e.stopPropagation();
-      //   e.preventDefault();
-      // });
-      //
-      // // Prevent multiple dropdowns being open at once.
-      // // First level.
-      // $('.cd-nav > .menu > .expanded > a').on('click', function (e) {
-      //   $('.cd-nav > .menu > .expanded > a').not(this).parent().removeClass('open');
-      //   $(this).parent().toggleClass('open');
-      // });
-      // // Second level.
-      // $('.cd-nav .menu .menu > .expanded > a').on('click', function (e) {
-      //   $(this).parent().toggleClass('open');
-      // });
-      //
-      // $('.dropdown').not(this).parent().removeClass('open');
+      // Media query event handler.
+      if (matchMedia) {
+        const mq = window.matchMedia("(min-width: 1024px)");
+        mq.addListener(WidthChange);
+        WidthChange(mq);
+      }
+
+      function WidthChange(mq) {
+        if (mq.matches) {
+
+          $('.cd-nav').show();
+
+        } else {
+
+          // Toggle mobile menu.
+          $('.cd-site-header__nav-toggle').on('click', function (e) {
+            $('.cd-nav').toggle();
+          });
+
+          // When lang switcher or ocha menu is open, close mobile menu.
+          $('.cd-language-switcher, .cd-ocha').on('show.bs.dropdown', function () {
+            $('.cd-nav').hide();
+          });
+
+        }
+      }
 
     }
   };
